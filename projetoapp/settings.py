483b29ac2e3b0b54b261f7671e249289ba27c94d
ruannegodoi <ruannegodoi@gmail.com>
2025-06-24 00:23:18 -0300

    Cria a app products e sua API

--- projetoapp/settings.py
+++ projetoapp/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'categories.apps.CategoriesConfig',
+    'products.apps.ProductsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
