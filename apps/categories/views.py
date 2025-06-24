304dc581613b6b783365253ffffebd4a5a9d25bd
ruannegodoi <ruannegodoi@gmail.com>
2025-06-24 00:16:02 -0300

    Cria a app products e sua API

---
+++ apps/categories/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .models import Category
+from .serializer import CategorySerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
